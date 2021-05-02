b8186c40539b00482ae6299bd2267e01197531a0
adipio <adipio@kent.edu>
2021-05-02 14:07:58 -0400

    Adding password functionality
    Checking the strength of passwords and hashing them.

---
+++ main.py
@@ -0,0 +1,49 @@
+import re
+from passlib.hash import bcrypt
+
+def passwordChecker(password):
+    #check the password length
+    if len(password) < 8:
+        return False
+
+    #checks for lower
+    lowerCheck = re.compile(r'[a-z]')
+    if not(lowerCheck.search(password)):
+        return False
+
+    #checks for upper
+    upperCheck = re.compile(r'[A-Z]')
+    if not(upperCheck.search(password)):
+        return False
+
+    #checks for number
+    numCheck = re.compile(r'\d')
+    if not(numCheck.search(password)):
+        return False
+
+    #checks for special character
+    spCheck = re.compile(r'\W')
+    if not(spCheck.search(password)):
+        return False
+
+    else:
+        return True
+
+def passHash(password):
+    hashed_password = bcrypt.hash(password)
+    return hashed_password
+
+def passwordCheck(userInput, password):
+    if (bcrypt.verify(userInput, password) == True): 
+        print("login sucsess")
+        return True
+    else:
+        print("login failed")
+        return False
+
+if __name__ == '__main__':
+    password = input("Enter your password.")
+    if passwordChecker(password) is True:
+        print("That is a strong password. ")
+    else:
+        print("That is a weak password.")
