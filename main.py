744cc0834b5ac9536d4dea645999da2ec7b938be
Dezimieren <greg.combass@gmail.com>
2021-05-12 17:54:12 -0400

    Added additional functions for salting, lists, and adding to log file. AddPasswordToLog is incomplete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import hashlib
-import uuid
+import uuid #Moving to replace with bcrypt
+import bcrypt
 
 def passwordChecker(password):
     #check the password length
@@ -30,9 +31,31 @@ def passwordChecker(password):
     else:
         return True
 
+def generateSalt():
+    salt = bcrypt.gensalt()
+    return salt
+
+def saltByteToString(salt):
+    stringSalt = salt.decode("utf-8")
+    return stringSalt
+
 def passHash(password):
     hashed_password = hashlib.sha512(password.encode())
     return hashed_password
+
+def previousPasswordExists(hashed_password):
+    list = open("previous_password.txt").readlines()
+
+    if hashed_password in list:
+        return True
+    else:
+        return False
+
+def addPasswordToLog(hashed_password):
+    with open("previous_password.txt", "a") as prev:
+        #prevLen = len(prev)
+        if prevLen < 8 or (prevLen == ""):
+            prev.write(user + ":" + hashed_password + "::0:90:7:::" + saltByteToString(salt) + "\n")
 
 if __name__ == '__main__':
     user = input("Enter you user name: ")
@@ -43,11 +66,20 @@ if __name__ == '__main__':
     else:
         print("That is a weak password.")
 
-    #salt = uuid.uuid4().hex
-    salt = "^AN:~fGRGX?t,/4s"
-    hashed_password = hashlib.sha512(password.encode() + salt.encode()).hexdigest()
+    #salt = uuid.uuid4().hex #Suggested to not use, while unique, they aren't random
+    #salt = "^AN:~fGRGX?t,/4s" #Static debug Salt
+    salt = generateSalt()
+
+    hashed_password = hashlib.sha512(password.encode() + salt).hexdigest()
 
     with open("shadow.txt", "a") as shadow:
     #shadow.write(user + ":" + passHash(password).hexdigest() + "::0:90:7:::")
-        shadow.write(user + ":" + hashed_password + "::0:90:7:::"+salt + "\n")
+        shadow.write(user + ":" + hashed_password + "::0:90:7:::" + saltByteToString(salt) + "\n")
+
+    if (previousPasswordExists(hashed_password)):
+        print("Could not add to password log.")
+    else:
+        addPasswordToLog(hashed_password)
+        print("Added to file")
+        
 
